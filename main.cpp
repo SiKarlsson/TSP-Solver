8709559539a15abd4fd211b05a3c1d605e128179
Simon Karlsson <simonkarlsson@msn.com>
2015-10-28 16:24:07 +0100

    Whatever

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 const bool printouts = false;
 
-int euclideanDistance(double p1[], double p2[])
+int euclideanDistance(double x1, double y1, double x2, double y2)
 {
-    double distSum = pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2);
+    double distSum = pow(x1 - x2, 2) + pow(y1 - y2, 2);
 
     return round(sqrt(distSum));
 }
@@ -44,7 +44,7 @@ int main()
 
     double* X = new double[N];
     double* Y = new double[N];
-    double* I = new double[N];
+    double* path = new double[N];
 
     string x;
     string y;
@@ -54,14 +54,63 @@ int main()
 
         X[i] = stod(x);
         Y[i] = stod(y);
-        I[i] = i;
+        path[i] = i;
 
         if (printouts) {
             cout << "x: " << X[i] << ", y: " << Y[i] << endl;
         }
     }
 
-    printIndexes(I, N);
+    // 2-opt
+    int totalDistance = 0;
+    
+    for (int i = 0; i < N - 1; i++) {
+        totalDistance += euclideanDistance(X[i], Y[i], X[i + 1], Y[i + 1]);
+    }
+
+    totalDistance += euclideanDistance(X[N - 1], Y[N - 1], X[0], Y[0]);
+
+    bool change;  
+
+    // for all edge pairs in current path
+    while (true) {
+        change = false;
+        for (int j = 0; j < N - 1; j++) {
+            int prev = j - 1;
+            if (prev < 0) {
+                prev = N - 1;
+            }
+            int next = j + 1;
+            int nextnext = j + 2;
+            if (nextnext > N - 1) {
+                nextnext = 0;
+            }
+            if (next > N - 1) {
+                next = 0;
+                nextnext = 1;
+            }
+            int newDistance = totalDistance 
+                - euclideanDistance(X[prev], Y[prev], X[j], Y[j]) 
+                - euclideanDistance(X[nextnext], Y[nextnext], X[next], Y[next]) 
+                + euclideanDistance(X[prev], Y[prev], X[next], Y[next]) 
+                + euclideanDistance(X[j], Y[j], X[nextnext], Y[nextnext]);
+            if (newDistance < totalDistance) {
+                totalDistance = newDistance;
+                swapEdges(&path, j, next);
+                swapEdges(&X, j, next);
+                swapEdges(&Y, j, next);
+                change = true;
+                break;
+            }
+        }
+        if (change) {
+            continue;
+        } else {
+            break;
+        }
+    }
+
+    printIndexes(path, N);
 
     return 0;
 }
