22597dd15edd812cc1cb8e5671d068996a6d8d74
William Perkola <perkola.william@gmail.com>
2015-10-28 13:21:55 +0100

    Develop

--- main.cpp
+++ main.cpp
@@ -11,8 +11,25 @@ const bool printouts = true;
 int euclideanDistance(double p1[], double p2[])
 {
     double distSum = pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2);
-    
+
     return round(sqrt(distSum));
+}
+
+void swapEdges(double* coordinates[][2], int a, int b)
+{
+    double temp[2];
+    temp[0] = *coordinates[a][0];
+    temp[1] = *coordinates[a][1];
+    *coordinates[a] = *coordinates[b];
+    *coordinates[b] = temp;
+}
+
+void printIndexes(double coordinates[][2], int N)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        cout << i << endl;
+    }
 }
 
 int main()
@@ -35,13 +52,19 @@ int main()
     for (int i = 0; i < N; ++i) {
         cin >> x >> y;
 
-        coordinates[i][0] = stod(x);
-        coordinates[i][1] = stod(y);
+        (coordinates)[i][0] = stod(x);
+        (coordinates)[i][1] = stod(y);
 
         if (printouts) {
-            cout << "x: " << coordinates[i][0] << ", y: " << coordinates[i][1] << endl;
+            cout << "x: " << (coordinates)[i][0] << ", y: " << (coordinates)[i][1] << endl;
         }
     }
+
+    printIndexes(coordinates, N);
+
+    swapEdges(coordinates, 0, 1);
+
+    printIndexes(coordinates, N);
 
     return 0;
 }
