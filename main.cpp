ec31e06bf2f50f1735b48f92e315500145487168
William Perkola <perkola.william@gmail.com>
2015-11-01 16:34:07 +0100

    Kneighbour2opt

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <limits.h>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -44,7 +45,6 @@ inline std::vector<int> twoOpt(std::vector<double> X, std::vector<double> Y, std
     int new_edge, old_edge, prev, next;
 
     while (change) {
-        change = false;
         for (int j = 0; j < N; ++j) {
             for (int k = (j + 1); k < N - 1; ++k) {
                 prev = j - 1;
@@ -129,6 +129,62 @@ inline std::vector<int> nearestNeighbour(std::vector<double> X, std::vector<doub
     return tour;
 }
 
+inline std::vector<int> twoOptNn(std::vector<int> tour, std::vector<std::vector<int> > distances, std::vector<std::vector<int> > nn, int min, int max)
+{
+    int N = tour.size();
+    int K = nn[0].size();
+
+    if (N < 4) {
+        return tour;
+    }
+
+    bool change = true;
+
+    int new_edge, old_edge, prev, next;
+
+    while (change) {
+        change = false;
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < K; ++j)
+            {
+                prev = (i-1 < 0) ? N-1 : i-1;
+                next = nn[i][j] + 1;
+                next = (next > N - 1) ? 0 : next;
+
+                if (tour[i] == tour[prev]) {
+                    continue;
+                }
+
+                if (tour[i] == tour[nn[i][j]] || tour[next] == tour[prev]) {
+                    continue;
+                }
+
+                if (distances[tour[prev]][tour[nn[i][j]]] + min > distances[tour[prev]][tour[i]] + max) {
+                    break;
+                }
+
+                if (DEBUG) {
+                    cout << "kollar på " << i << "-" << prev << " och " << nn[i][j] << "-" << next << endl;
+                    //cout << "me värden " << tour[i] << "-" << tour[prev] << " och " << tour[nn[i][j]] << "-" << tour[next] << endl;
+                }
+
+                old_edge = distances[tour[prev]][tour[i]] + distances[tour[nn[i][j]]][tour[next]];
+                new_edge = distances[tour[i]][tour[next]] + distances[tour[nn[i][j]]][tour[prev]];
+
+                if (new_edge < old_edge) {
+                    tour = swapEdges(tour, i, nn[i][j]);
+                    change = true;
+                    max = std::max(max, std::max(distances[tour[prev]][tour[nn[i][j]]], distances[tour[i]][tour[next]]));
+                    break;
+                }
+            }
+        }
+    }
+
+    return tour;
+}
+
 int main()
 {
     int N;
@@ -155,13 +211,72 @@ int main()
     {
         distances[i].resize(N);
     }
+    int min = INT_MAX;
+    int max = 0;
     for (int i = 0; i < N; ++i) {
         for (int j = i; j < N; ++j) {
             if (i != j) {
                 ed = euclideanDistance(X[i], Y[i], X[j], Y[j]);
                 distances[i][j] = ed;
                 distances[j][i] = ed;
+                min = std::min(min, ed);
+                max = std::max(max, ed);
             }
+        }
+    }
+
+    if (DEBUG) {
+        cout << "MIN DISTANCE = " << min << endl;
+        cout << "MAX DISTANCE = " << max << endl;
+    }
+
+    int K = 20;
+    K = std::min(K, N)-1;
+
+    typedef pair<pair<int, int>, int> adj;
+
+    auto comp = [](adj a, adj b) {
+        return a.second > b.second;
+    };
+
+    std::vector<std::vector<int> > nn(N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::vector<adj> adjs(N);
+
+        for (int j = 0; j < N; ++j)
+        {
+            adjs[j] = adj(pair<int, int>(i, j), distances[i][j]);
+        }
+
+        std::priority_queue<adj, std::vector<adj>, decltype(comp) > queue {comp, std::move(adjs)};
+
+        for (int j = 0; j < (K + 1); ++j)
+        {
+            adj test = queue.top();
+            if (test.first.first != test.first.second) {
+                //nn[i].push_back(test.first.second);
+                nn[i].push_back(test.first.second);
+                if (DEBUG) {
+                    cout << "[" << test.first.first << ", " << test.first.second << "] = " << test.second << " ";
+                }
+            }
+            queue.pop();
+        }
+        if (DEBUG) {
+            cout << endl;
+        }
+    }
+
+    if (DEBUG) {
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < K; ++j)
+            {
+                cout << nn[i][j] << " ";
+            }
+            cout << endl;
         }
     }
 
@@ -169,7 +284,9 @@ int main()
 
     tour = nearestNeighbour(X, Y, tour, distances);
 
-    tour = twoOpt(X, Y, tour, distances);
+    //tour = twoOpt(X, Y, tour, distances);
+
+    tour = twoOptNn(tour, distances, nn, min, max);
 
     for (int i = 0; i < tour.size(); ++i)
     {
